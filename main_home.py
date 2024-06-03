537c452aa2fc65880c29915faa4797a048379914
ubuntu <piotr.perz@wp.pl>
2024-06-03 22:56:17 +0200

    Day 31: Show station table on the homepage

--- main_home.py
+++ main_home.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
-
+df = pd.read_csv("dictionary.csv")
 @app.route("/")
 def home():
     return render_template("home_home.html")
 
 @app.route("/api/v1/<word>")
 def api(word):
-    return {"definition": str(word).upper(),
+    definition = df.loc[df['word'] == word]['definition'].squeeze()
+    print(df)
+    return {"definition": definition,
             "word": word}
 
 if __name__ == "__main__":
